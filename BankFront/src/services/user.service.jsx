aa80bfb134b81d5d7cddc93e89d4d21cbb1372fe
KayDaaH <thomas36.rolland@gmail.com>
2023-05-25 06:18:11 +0200

    Fin

--- BankFront/src/services/user.service.jsx
+++ BankFront/src/services/user.service.jsx
@@ -6,26 +6,22 @@ import Axios from "./caller.service";
  * @returns {Object|boolean} - User data object if successful, otherwise false.
  */
 const getUserData = async (credentials) => {
-  try {
-    const responseLogin = await Axios.post("/login", credentials);
-    const token = responseLogin.data.body.token;
-    if (token) {
-      const responseData = await Axios.post("/profile", null, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const userData = {
-        token: token,
-        firstName: responseData.data.body.firstName,
-        lastName: responseData.data.body.lastName,
-        userId: responseData.data.body.id,
-      };
-      return userData;
-    } else {
-      return false;
-    }
-  } catch (error) {
+  const responseLogin = await Axios.post("/login", credentials);
+  const token = responseLogin.data.body.token;
+  if (token) {
+    const responseData = await Axios.post("/profile", null, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const userData = {
+      token: token,
+      firstName: responseData.data.body.firstName,
+      lastName: responseData.data.body.lastName,
+      userId: responseData.data.body.id,
+    };
+    return userData;
+  } else {
     return false;
   }
 };
